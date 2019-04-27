2ee05791d09393ade3c46fcd1e593f8a5002cf26
Warayut Taekrathok <warayut2555@gmail.com>
2019-04-27 16:05:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,78 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>VIVO SHOP</title>
+
+    <!-- Bootstrap CSS 4.3 -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 </head>
+
 <body>
-    
+
+
+    <div class="container">
+
+        <header class="blog-header py-3">
+            <img src="./imgs/banner.jpg" width="100%" height="" alt="Responsive image">
+        </header>
+
+
+        <div class="nav-scroller py-1 mb-2">
+            <nav class="nav d-flex justify-content-between">
+                <a class="p-2 text-muted" href="#"></a>
+                <a class="p-2 text-muted" href="#">หน้าหลัก</a>
+                <a class="p-2 text-muted" href="#">ข้อมูลสมาชิก</a>
+                <a class="p-2 text-muted" href="#">รายการสินค้า</a>
+                <a class="p-2 text-muted" href="#">การชำระเงิน</a>
+                <a class="p-2 text-muted" href="#">รายงาน</a>
+                <a class="p-2 text-muted" href="#"></a>
+            </nav>
+        </div>
+
+        <div>
+            <h1>My first styled page</h1>
+
+            <p>Welcome to my styled page!</p>
+
+            <p>It lacks images, but at least it has style.
+                And it has links, even if they don't go
+                anywhere&hellip;</p>
+
+            <p>There should be more here, but I don't know
+                what yet.</p>
+
+            <!-- Sign and date the page, it's only polite! -->
+            <address>Made 5 April 2004<br>
+                by myself.
+            </address>
+        </div>
+
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
+
+<style>
+body {
+    background-color: initial;
+}
+</style>
