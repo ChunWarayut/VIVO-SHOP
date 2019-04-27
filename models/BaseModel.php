6ab0a18ae1c3b44e7d072768871b0a81397ec291
Warayut Taekrathok <warayut2555@gmail.com>
2019-04-27 15:00:05 +0700

    Create BaseModel.php

---
+++ models/BaseModel.php
@@ -0,0 +1,20 @@
+<?php
+abstract class BaseModel{
+    public static $db;
+    protected $host="localhost";
+    protected $db_name="vivo-shop";
+    protected $username="root";
+	protected $password="root123456";
+	function __construct(){
+        static::$db = mysqli_connect($host, $username, $password, $db_name);
+        if (mysqli_connect_errno())
+        {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        }
+    }
+}
+function convertImg($name){
+	$data = iconv("utf-8", "tis-620",$name);
+	return $data;
+}
+?>
